6a85379c30be334515bb5faad111cb052f36d47d
Hiago de Sousa Patricio <hiagodesousa13@gmail.com>
2025-04-03 19:31:46 -0400

    First commit

---
+++ src/main/java/gov/mt/seplag/gestao_servidores/GestaoDeServidoresApplication.java
@@ -0,0 +1,13 @@
+package gov.mt.seplag.gestao_servidores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeServidoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeServidoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/mt/seplag/gestao_servidores/GestaoDeServidoresApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.mt.seplag.gestao_servidores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeServidoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
