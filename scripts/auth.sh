b98b0c49972d9ebb70fcfadf0fe872ffb3e53b8f
Hiago de Sousa Patricio <hiagodesousa13@gmail.com>
2025-04-09 05:21:54 -0400

    Feat: script de autenticação

---
+++ scripts/auth.sh
@@ -0,0 +1,19 @@
+curl -X 'POST' \
+  'http://localhost:8080/api/auth/register' \
+  -H 'accept: */*' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "username": "joao_das_neves",
+  "password": "12345678"
+}' | jq
+
+echo "\n"
+
+curl -X 'POST' \
+  'http://localhost:8080/api/auth/login' \
+  -H 'accept: */*' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "username": "joao_das_neves",
+  "password": "12345678"
+}' | jq
